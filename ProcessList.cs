d37740e5aa39ae84f606205a3396712ceed6439d
Simon Taylor <simontaylor81@hotmail.com>
2014-01-10 21:08:05 +0000

    Initial commit

---
+++ ProcessList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RunInTray
+{
+	class ProcessList
+	{
+		// Run a process.
+		public void RunProcess(string path)
+		{
+			// Run the file with a hidden window.
+			var startInfo = new ProcessStartInfo(path);
+			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			startInfo.UseShellExecute = true;
+
+			processes.Add(Process.Start(startInfo));
+		}
+
+		// Close all processes, forcefully if necessary.
+		public void CloseAll()
+		{
+			foreach (var process in processes)
+			{
+				if (process.CloseMainWindow())
+				{
+					// Successfully sent close message, wait for the process to exit.
+					if (!process.WaitForExit(2000))
+					{
+						// Process is not responding -- kill it.
+						process.Kill();
+					}
+				}
+				else
+				{
+					// Could not close the app nicely, so kill it.
+					process.Kill();
+				}
+
+				process.Close();
+			}
+
+			processes.Clear();
+		}
+
+		// Get the friendly names for each process.
+		public IEnumerable<string> GetNames()
+		{
+			return processes.Select(p => p.ProcessName);
+		}
+
+		// Do we have any running processes?
+		public bool HasProcesses()
+		{
+			return processes.Any();
+		}
+
+		private List<Process> processes = new List<Process>();
+	}
+}
---
+++ RunInTrayApp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunInTray
+{
+	public class RunInTrayApp : Form
+	{
+		[STAThread]
+		public static void Main()
+		{
+			Application.Run(new RunInTrayApp());
+		}
+
+		private NotifyIcon trayIcon;
+		private ContextMenu trayMenu;
+		private ProcessList processes;
+
+		public RunInTrayApp()
+		{
+			// Create tray menu.
+			trayMenu = new ContextMenu();
+			trayMenu.MenuItems.Add("Exit", OnExit);
+			trayMenu.MenuItems.Add("Run App", OnRunApp);
+
+			// Create a tray icon. In this example we use a
+			// standard system icon for simplicity, but you
+			// can of course use your own custom icon too.
+			trayIcon = new NotifyIcon();
+			trayIcon.Text = "Run-in-tray";
+			trayIcon.Icon = new Icon(SystemIcons.Application, 16, 16);
+
+			// Add menu to tray icon and show it.
+			trayIcon.ContextMenu = trayMenu;
+			trayIcon.Visible = true;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			Visible = false; // Hide form window.
+			ShowInTaskbar = false; // Remove from taskbar.
+
+			base.OnLoad(e);
+		}
+
+		private void OnExit(object sender, EventArgs e)
+		{
+			if (processes.HasProcesses())
+			{
+				// Warn if processes are running.
+				var message = "The following processes are still running. Are you sure you wish to exit?\n  "
+					+ string.Join("\n  ", processes.GetNames());
+				var result = MessageBox.Show(message, "Run in tray", MessageBoxButtons.YesNo);
+				if (result == DialogResult.No)
+				{
+					return;
+				}
+				
+				// Quit all running processes.
+				processes.CloseAll();
+			}
+
+			Application.Exit();
+		}
+
+		private void OnRunApp(object sender, EventArgs e)
+		{
+			// Ask the user to pick the application.
+			var dialog = new OpenFileDialog();
+			dialog.InitialDirectory = "C:\\";
+			dialog.Filter = "All files (*.*)|*.*";
+			dialog.RestoreDirectory = true;
+
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				processes.RunProcess(dialog.FileName);
+			}
+		}
+
+		protected override void Dispose(bool isDisposing)
+		{
+			if (isDisposing)
+			{
+				// Release the icon resource.
+				trayIcon.Dispose();
+			}
+
+			base.Dispose(isDisposing);
+		}
+	}
+}
